f18228b2dd84b32365ebc22d743331b87d9dc083
Beyley Thomas <ep1cm1n10n123@gmail.com>
2024-09-17 23:26:22 -0700

    update to latest zig

--- build.zig
+++ build.zig
@@ -6,22 +6,20 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardOptimizeOption(.{});
 
-    const executable_options: std.build.ExecutableOptions = .{
+    var executable = b.addExecutable(.{
         .name = "fbv",
         .target = target,
         .optimize = mode,
-    };
-
-    var executable: *std.build.CompileStep = b.addExecutable(executable_options);
+    });
 
     executable.linkLibC();
 
-    executable.addIncludePath(root_path ++ "miniz/");
-    executable.addIncludePath(root_path ++ "libspng/spng");
+    executable.addIncludePath(b.path("miniz/"));
+    executable.addIncludePath(b.path("libspng/spng"));
 
-    executable.addCSourceFiles(srcs, &.{ "-std=c99", "-DSPNG_USE_MINIZ" });
+    executable.addCSourceFiles(.{ .files = srcs, .flags = &.{ "-std=c99", "-Wno-implicit-function-declaration", "-DSPNG_USE_MINIZ" } });
 
-    var config_header = b.addConfigHeader(.{ .include_path = "config.h" }, .{
+    const config_header = b.addConfigHeader(.{ .include_path = "config.h" }, .{
         // .FBV_SUPPORT_JPEG = 1,
         .FBV_SUPPORT_PNG = 1,
         .FBV_SUPPORT_BMP = 1,
@@ -30,25 +28,19 @@ pub fn build(b: *std.Build) !void {
 
     executable.addConfigHeader(config_header);
 
-    executable.install();
+    b.installArtifact(executable);
 }
-
-fn root() []const u8 {
-    return std.fs.path.dirname(@src().file) orelse ".";
-}
-
-const root_path = root() ++ "/";
 
 const srcs = &.{
-    root_path ++ "main.c",
-    root_path ++ "vt.c",
-    root_path ++ "bmp.c",
-    root_path ++ "miniz/miniz.c",
-    root_path ++ "miniz/miniz_tinfl.c",
-    root_path ++ "miniz/miniz_tdef.c",
-    root_path ++ "libspng/spng/spng.c",
-    root_path ++ "png.c",
-    root_path ++ "jpeg.c",
-    root_path ++ "fb_display.c",
-    root_path ++ "transforms.c",
+    "main.c",
+    "vt.c",
+    "bmp.c",
+    "miniz/miniz.c",
+    "miniz/miniz_tinfl.c",
+    "miniz/miniz_tdef.c",
+    "libspng/spng/spng.c",
+    "png.c",
+    "jpeg.c",
+    "fb_display.c",
+    "transforms.c",
 };
